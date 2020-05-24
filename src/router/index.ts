451ff44e2fbe735cd4c5e4e5560944ae2f3c25dd
Mikjail Salazar <mikjailsalazar@gmail.com>
2020-05-24 22:56:17 +0400

    Config - Fixing project architecture (adding artifact folder view)

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
 import Home from '../views/Home.vue';
-import ProductBacklog from '../views/ProductBacklog.vue';
-import SprintBacklog from '../views/SprintBacklog.vue';
+import ProductBacklog from '../views/artifacts/ProductBacklog.vue';
+import SprintBacklog from '../views/artifacts/SprintBacklog.vue';
+import Increment from '../views/artifacts/Increment.vue';
+import ScrumMaster from '../views/roles/ScrumMaster.vue';
 
 Vue.use(VueRouter);
 
@@ -21,6 +23,16 @@ const routes: Array<RouteConfig> = [
     path: '/sprint-backlog',
     name: 'Sprint Backlog',
     component: SprintBacklog
+  },
+  {
+    path: '/product-increment',
+    name: 'Increment',
+    component: Increment
+  },
+  {
+    path: '/scrum-master',
+    name: 'Scrum Master',
+    component: ScrumMaster
   }
 ];
 
